af2f2a82aca3fc907515fa7ce625abbdf0464f44
seeu <eddoit@outlook.com>
2025-02-19 23:23:06 +0100

    integral type

--- MicrosoftLearnCsharp/Program.cs
+++ MicrosoftLearnCsharp/Program.cs
@@ -1,5 +1,5 @@
 ﻿// the ourAnimals array will store the following: 
-
+/*
 string animalSpecies = "";
 string animalID = "";
 string animalAge = "";
@@ -327,8 +327,8 @@ do
             break;
     }
 } while (appRunning);
-
-
+*/
+/*
 //string[][] jaggedArray = new[]
 //{
 //    new[] { "one1", "two1", "three1", "four1", "five1", "six1" },
@@ -349,6 +349,29 @@ do
 //    }
 //    Console.WriteLine();
 //}
+*/
 
 
 
+
+Console.WriteLine("Signed integral types:");
+// // "Signed integral types:"
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
