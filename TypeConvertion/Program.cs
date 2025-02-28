4dae5e11adeb58f661bf865f817101aeff3ca85b
seeu <eddoit@outlook.com>
2025-03-01 00:11:19 +0100

    added python file

---
+++ TypeConvertion/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Type Conversion";
+
+
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
+
+Console.WriteLine();
+
+int myInt = 3;
+Console.WriteLine(myInt);
+
+decimal myDesimal = myInt;
+Console.WriteLine(myDesimal);
+
+Console.WriteLine();
+
+decimal newDecimal = 3.14m;
+Console.WriteLine($"This is decimals: {newDecimal}");
+
+int newInt = (int)newDecimal;
+Console.WriteLine($"This is int: {newInt}");
