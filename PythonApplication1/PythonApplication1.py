fb737d5da4a5380114a90cc9c9c6de72084a369c
seeu <eddoit@outlook.com>
2025-04-12 00:24:20 +0200

    Remove characters

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,17 +1,17 @@
-from module1 import conversion, sayHello
-sayHello()
+# from module1 import conversion, sayHello
+# sayHello()
 
-print("\n\tRunning Python Code\n")
+# print("\n\tRunning Python Code\n")
 
-myDecimal = 2.14
-print(f"This is decimal: {myDecimal}")
+# myDecimal = 2.14
+# print(f"This is decimal: {myDecimal}")
 
-myInt = myDecimal
-print(f"This is integer: {myInt:02}")
+# myInt = myDecimal
+# print(f"This is integer: {myInt:02}")
 
 
-my_result = conversion("2", "3")
-print(f"Converted string to Integer: {my_result}")
+# my_result = conversion("2", "3")
+# print(f"Converted string to Integer: {my_result}")
 
 """
 1.	data[:5] extracts the substring from the start of the string up to (but not including) index 5.
@@ -22,3 +22,5 @@ data = "12345John Smith          5000  3  "
 print(data)
 updated_data = data[:5] + data[25:]  # Remove characters from index 5 to 25
 print(updated_data)
+new_data = data.replace("12345", "")
+print(new_data.replace("5000  3", ""))
