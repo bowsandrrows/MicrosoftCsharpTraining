167b09b1f1bd3af984613e30388b1ee48d17bfee
seeu <eddoit@outlook.com>
2025-04-11 22:39:29 +0200

    compairing python code snipet to C#s

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -14,3 +14,7 @@ my_result = conversion("2", "3")
 print(f"Converted string to Integer: {my_result}")
 
 
+data = "12345John Smith          5000  3  "
+print(data)
+updated_data = data[:5] + data[25:]  # Remove characters from index 5 to 25
+print(updated_data)
