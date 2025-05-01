730bec99c5c5f37bb6c99b7a0635f28b90e5a0da
seeu <eddoit@outlook.com>
2025-05-02 01:03:30 +0200

    changed animations icons and speed

--- MicrosoftLearnCsharp/Program.cs
+++ MicrosoftLearnCsharp/Program.cs
@@ -16,12 +16,12 @@ string menuSelection = "";
 decimal decimalDonation = 0.00m;
 
 // array used to store runtime data
-string[,] ourAnimals = new string[maxPets, 7];
+string[,] ourAnimals = new string[ maxPets, 7 ];
 
 // sample data ourAnimals array entries
-for (int i = 0; i < maxPets; i++)
+for ( int i = 0; i < maxPets; i++ )
 {
-    switch (i)
+    switch ( i )
     {
         case 0:
             animalSpecies = "dog";
@@ -62,6 +62,7 @@ for (int i = 0; i < maxPets; i++)
             animalNickname = "lion";
             suggestedDonation = "";
 
+
             break;
 
         default:
@@ -76,17 +77,18 @@ for (int i = 0; i < maxPets; i++)
 
     }
 
-    ourAnimals[i, 0] = "ID #: " + animalID;
-    ourAnimals[i, 1] = "Species: " + animalSpecies;
-    ourAnimals[i, 2] = "Age: " + animalAge;
-    ourAnimals[i, 3] = "Nickname: " + animalNickname;
-    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
-    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
-    
-    if (!decimal.TryParse(suggestedDonation, out decimalDonation)){
+    ourAnimals[ i, 0 ] = "ID #: " + animalID;
+    ourAnimals[ i, 1 ] = "Species: " + animalSpecies;
+    ourAnimals[ i, 2 ] = "Age: " + animalAge;
+    ourAnimals[ i, 3 ] = "Nickname: " + animalNickname;
+    ourAnimals[ i, 4 ] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[ i, 5 ] = "Personality: " + animalPersonalityDescription;
+
+    if ( !decimal.TryParse( suggestedDonation, out decimalDonation ) )
+    {
         decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
     }
-    ourAnimals[i, 6] = $"Suggested Donation: {decimalDonation:C2}";
+    ourAnimals[ i, 6 ] = $"Suggested Donation: {decimalDonation:C2}";
 }
 
 // top-level menu options
@@ -95,36 +97,36 @@ do
     // NOTE: the Console.Clear method is throwing an exception in debug sessions
     Console.Clear();
 
-    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-    Console.WriteLine(" 1. List all of our current pet information");
-    Console.WriteLine(" 2. Display all dogs with a specified characteristic");
+    Console.WriteLine( "Welcome to the Contoso PetFriends app. Your main menu options are:" );
+    Console.WriteLine( " 1. List all of our current pet information" );
+    Console.WriteLine( " 2. Display all dogs with a specified characteristic" );
     Console.WriteLine();
-    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+    Console.WriteLine( "Enter your selection number (or type Exit to exit the program)" );
 
     readResult = Console.ReadLine();
-    if (readResult != null)
+    if ( readResult != null )
     {
         menuSelection = readResult.ToLower();
     }
 
     // switch-case to process the selected menu option
-    switch (menuSelection)
+    switch ( menuSelection )
     {
         case "1":
             // list all pet info
-            for (int i = 0; i < maxPets; i++)
+            for ( int i = 0; i < maxPets; i++ )
             {
-                if (ourAnimals[i, 0] != "ID #: ")
+                if ( ourAnimals[ i, 0 ] != "ID #: " )
                 {
                     Console.WriteLine();
-                    for (int j = 0; j < 7; j++)
+                    for ( int j = 0; j < 7; j++ )
                     {
-                        Console.WriteLine(ourAnimals[i, j].ToString());
+                        Console.WriteLine( ourAnimals[ i, j ].ToString() );
                     }
                 }
             }
 
-            Console.WriteLine("\r\nPress the Enter key to continue");
+            Console.WriteLine( "\r\nPress the Enter key to continue" );
             readResult = Console.ReadLine();
 
             break;
@@ -134,12 +136,12 @@ do
 
             string dogCharacteristic = "";
 
-            while (dogCharacteristic == "")
+            while ( dogCharacteristic == "" )
             {
                 // #2 have user enter multiple comma separated characteristics to search for
-                Console.WriteLine($"\r\nEnter one desired dog characteristic to search for");
+                Console.WriteLine( $"\r\nEnter one desired dog characteristic to search for" );
                 readResult = Console.ReadLine();
-                if (readResult != null)
+                if ( readResult != null )
                 {
                     dogCharacteristic = readResult.ToLower().Trim();
                     Console.WriteLine();
@@ -150,37 +152,41 @@ do
             string dogDescription = "";
 
             // #4 update to "rotating" animation with countdown
-            string[] searchingIcons = [".  ", ".. ", "..."];
+            string[] searchingIcons = [ "2  ", "1 ", "0" ];
+            string[] searchingIcons2 = { " |", " /", "--", " \\", " *" };
 
             // loop ourAnimals array to search for matching animals
-            for (int i = 0; i < maxPets; i++)
+            for ( int i = 0; i < maxPets; i++ )
             {
 
-                if (ourAnimals[i, 1].Contains("dog"))
+                if ( ourAnimals[ i, 1 ].Contains( "dog" ) )
                 {
-                    
+
                     // Search combined descriptions and report results
-                    dogDescription = ourAnimals[i, 4] + "\r\n" + ourAnimals[i, 5];
-                    
-                    for (int j = 5; j > -1 ; j--)
+                    dogDescription = ourAnimals[ i, 4 ] + "\r\n" + ourAnimals[ i, 5 ];
+
+                    for ( int j = 5; j > -1; j-- )
                     {
-                    // #5 update "searching" message to show countdown 
-                        foreach (string icon in searchingIcons)
+                        // #5 update "searching" message to show countdown 
+                        foreach ( string icon in searchingIcons )
                         {
-                            Console.Write($"\rsearching our dog {ourAnimals[i, 3]} for {dogCharacteristic} {icon}");
-                            Thread.Sleep(250);
+                            foreach ( var icon2 in searchingIcons2 )
+                            {
+                                Console.Write( $"\rsearching our dog {ourAnimals[ i, 3 ]} for {dogCharacteristic} {icon2} {icon}" );
+                                Thread.Sleep( 150 );
+                            }
                         }
-                        
-                        Console.Write($"\r{new String(' ', Console.BufferWidth)}");
+
+                        Console.Write( $"\r{new String( ' ', Console.BufferWidth )}" );
                     }
-                    
+
                     // #3a iterate submitted characteristic terms and search description for each term
-                    
-                    if (dogDescription.Contains(dogCharacteristic))
+
+                    if ( dogDescription.Contains( dogCharacteristic ) )
                     {
                         // #3b update message to reflect term 
                         // #3c set a flag "this dog" is a match
-                        Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
+                        Console.WriteLine( $"\nOur dog {ourAnimals[ i, 3 ]} is a match!" );
 
                         noMatchesDog = false;
                     }
@@ -189,12 +195,12 @@ do
                 }
             }
 
-            if (noMatchesDog)
+            if ( noMatchesDog )
             {
-                Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+                Console.WriteLine( "None of our dogs are a match found for: " + dogCharacteristic );
             }
 
-            Console.WriteLine("\n\rPress the Enter key to continue");
+            Console.WriteLine( "\n\rPress the Enter key to continue" );
             readResult = Console.ReadLine();
 
             break;
@@ -203,7 +209,7 @@ do
             break;
     }
 
-} while (menuSelection != "exit");
+} while ( menuSelection != "exit" );
 
 
 
