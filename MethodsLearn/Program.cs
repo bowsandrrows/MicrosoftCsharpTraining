dc42af3465893505b8e9d9be6a288b8b2bcff114
seeu <eddoit@outlook.com>
2025-05-08 04:20:01 +0200

    intro to a new virsion starts on line 75

--- MethodsLearn/Program.cs
+++ MethodsLearn/Program.cs
@@ -37,7 +37,7 @@ else
 
 */
 
-//Console.WriteLine( "\nGuese 5 numbers from 1 to 100.\n5 numbers = $10000, 4 numbers = $2000, 3 numbers = $100. " );
+//Console.WriteLine( "\nGuess 5 numbers from 1 to 100.\n5 numbers = $10000, 4 numbers = $2000, 3 numbers = $100. " );
 //Console.WriteLine( "To quit at any time type Q/q" );
 //int guessedNumbers = 1;
 //int currentNumber = 1;
@@ -71,40 +71,39 @@ else
 //}
 
 
- 
-Console.WriteLine("\nGuess 5 numbers from 1 to 100.");
-Console.WriteLine("5 numbers = $10000, 4 numbers = $2000, 3 numbers = $100.");
-Console.WriteLine("To quit at any time, type Q/q.");
+
+Console.WriteLine( "\nGuess 5 numbers from 1 to 100." );
+Console.WriteLine( "5 numbers = $10000, 4 numbers = $2000, 3 numbers = $100." );
+Console.WriteLine( "To quit at any time, type Q/q." );
 
 int pickedNumbers = 1;
 
-while (pickedNumbers <= 5)
+while ( pickedNumbers <= 5 )
 {
-    Console.Write($"Enter number {pickedNumbers}: ");
+    Console.Write( $"Enter number {pickedNumbers}: " );
     string? prompt = Console.ReadLine();
 
-    if (prompt?.ToLower() == "q")
+    if ( prompt?.ToLower() == "q" )
     {
         break;
     }
 
-    bool isValidNumber = int.TryParse(prompt, out int currentNumber);
+    bool isValidNumber = int.TryParse( prompt, out int currentNumber );
 
-    if (!isValidNumber)
+    if ( !isValidNumber )
     {
-        Console.WriteLine("Enter a number in digits.");
+        Console.WriteLine( "Enter a number in digits." );
         continue;  // Repeats the prompt instead of increasing `pickedNumbers`
     }
-    else if (currentNumber <= 0)
+    else if ( currentNumber <= 0 )
     {
-        Console.WriteLine("Number must be greater than zero.");
+        Console.WriteLine( "Number must be greater than zero." );
         continue;  // Again, retries input
     }
 
-    pickedNumbers++; 
+    pickedNumbers++;
 }
- 
- 
+
 
 /*
 
