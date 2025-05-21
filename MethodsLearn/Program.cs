cb9b568b83bad688dbc4247970bd29d387a14795
seeu <eddoit@outlook.com>
2025-05-21 02:17:03 +0200

    updated ValidateLength and ValidateZeroe methods

--- MethodsLearn/Program.cs
+++ MethodsLearn/Program.cs
@@ -15,23 +15,44 @@ then ipAddress is valid
 else ipAddress is invalid
 */
 
+string ipv4Input = "107.31.1.5";
 
-if ( ValidateLength() && ValidateZeroes() && ValidateRange())
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+
+ValidateLength();
+ValidateZeroes();
+ValidateRange();
+
+if ( validLength && validZeroes && validRange )
 {
-    Console.WriteLine($"ip is a valid IPv4 address");
+    Console.WriteLine( $"ip is a valid IPv4 address" );
 }
 else
 {
-    Console.WriteLine($"ip is an invalid IPv4 address");
+    Console.WriteLine( $"ip is an invalid IPv4 address" );
 }
 
 void ValidateLength()
 {
-
+    string[] address = ipv4Input.Split(".");
+    validLength = address.Length == 4;
 }
 void ValidateZeroes()
 {
+    string[] address = ipv4Input.Split( "." );
 
+    foreach ( string number in address )
+    {
+        if ( number.Length > 1 && number.StartsWith( "0" ) )
+        {
+            validZeroes = false;
+            return;
+        }
+    }
+    validZeroes = true;
 }
 
 void ValidateRange()
