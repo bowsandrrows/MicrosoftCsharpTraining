f4877b879f2f3b682cbd6a277a2dbe675bcdfdcc
seeu <eddoit@outlook.com>
2025-06-03 17:09:59 +0200

    Understand method scope

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine( "\tCreate a method with parameters\n" );
+﻿Console.WriteLine( "\n\tCreate a method with parameters\n" );
 
 
 #region
-
+/*
 string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
 
 DisplayStudents( students );
@@ -15,6 +15,35 @@ void DisplayStudents( string[] students )
         Console.Write( $"{student}, " );
     }
     Console.WriteLine();
+}
+*/
+#endregion
+
+
+#region Understand method scope
+
+double pi = 3.14159;
+PrintCircleInfo( 12 );
+PrintCircleInfo( 24 );
+
+void PrintCircleInfo( int radius )
+{
+    Console.WriteLine($"Circle with a radius {radius}"); 
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+
+void PrintCircleArea( int radius )
+{
+    double area = pi * ( radius * radius );
+    Console.WriteLine( $"Area = {area}" );
+}
+
+
+void PrintCircleCircumference( int radius )
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine( $"Circumference = {circumference}" );
 }
 
 #endregion
