bfb83719ba9dd10c2b36fe4533d0dfc269188b69
seeu <eddoit@outlook.com>
2025-06-03 17:44:38 +0200

    Test pass by value, line 25 - 53

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine( "\n\tCreate a method with parameters\n" );
+﻿using System.Numerics;
+
+Console.WriteLine( "\n\tCreate a method with parameters\n" );
 
 
 #region
@@ -21,7 +23,7 @@ void DisplayStudents( string[] students )
 
 
 #region Understand method scope
-
+/*
 double pi = 3.14159;
 PrintCircleInfo( 12 );
 PrintCircleInfo( 24 );
@@ -45,5 +47,23 @@ void PrintCircleCircumference( int radius )
     double circumference = 2 * pi * radius;
     Console.WriteLine( $"Circumference = {circumference}" );
 }
-
+*/
 #endregion
+
+#region Test pass by value
+/*
+int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply( a, b, c );
+Console.WriteLine($"Global statement: {a} x {b} = {c}");
+
+static void Multiply( int a, int b, int c )
+{
+    c = a * b;
+    Console.WriteLine($"Inside Multiply method: {a} x {b} = {c}");
+}
+*/
+#endregion
+
