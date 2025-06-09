7b77b5dff190fa027c835dc49fff9e2d8068d519
seeu <eddoit@outlook.com>
2025-06-09 16:53:21 +0200

    ....

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -196,12 +196,39 @@ void CreateMail( string externalDomain = "hayworth.com", char at = '@' )
 
     for ( int i = 0; i < external.GetLength( 0 ); i++ )
     {
-        foreach ( var item in external )
-        {
-            Console.Write( $"{item[0]}");
-        }
-        Console.WriteLine();
     }
 }
 
 CreateMail();
+
+
+string[,] corporate =
+    {
+        {"Robert", "Bavin"}, {"Simon", "Bright"},
+        {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+        {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+string[,] external =
+{
+        {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+        {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+    };
+
+string externalDomain = "hayworth.com";
+
+for ( int i = 0; i < corporate.GetLength( 0 ); i++ )
+{
+    DisplayEmail( first: corporate[ i, 0 ], last: corporate[ i, 1 ] );
+}
+
+for ( int i = 0; i < external.GetLength( 0 ); i++ )
+{
+    DisplayEmail( first: external[ i, 0 ], last: external[ i, 1 ], domain: externalDomain );
+}
+
+void DisplayEmail( string first, string last, string domain = "contoso.com" )
+{
+    string email = first.Substring( 0, 2 ) + last;
+    email = email.ToLower();
+    Console.WriteLine( $"{email}@{domain}" );
+}
