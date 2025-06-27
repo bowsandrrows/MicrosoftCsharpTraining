a1abaec8ba3a8de6c3a66a24eee49d48358a6afd
seeu <eddoit@outlook.com>
2025-06-27 16:38:44 +0200

    Return numbers from methods

---
+++ ReturnNumbersFromMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine( "\tReturn numbers from methods\n" );
+
+
+#region Create a method that returns an integer
+
+double usd = 23.73;
+int vnd = UsdToVnd( usd );
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine( $"${vnd} VND = ${VndToUsd( vnd )} USD" );
+
+int UsdToVnd( double usd )
+{
+    int rate = 23500;
+    return (int) ( usd * rate );
+}
+
+double VndToUsd(int vnd )
+{
+    double rate = 23500;
+    return vnd / rate;
+}
+
+#endregion
+
+#region
+
+
+
+
+
+#endregion
