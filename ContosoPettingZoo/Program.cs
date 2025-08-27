9736d776aec4348f965d73fa462bbd5371c14a68
seeu <eddoit@outlook.com>
2025-08-27 19:29:08 +0200

    ...

--- ContosoPettingZoo/Program.cs
+++ ContosoPettingZoo/Program.cs
@@ -29,17 +29,34 @@ Setup
 */
 
 string[] pettingZoo =
-[
+{
     "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-];
+};
 
-// RandomizeAnimals();
+RandomizeAnimals();
+string[,] group = AssignGroup();
+Console.WriteLine( "School A" );
+// PrintGroup(group);
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for ( int i = 0; i < pettingZoo.Length; i++ )
+    {
+        int r = random.Next( i, pettingZoo.Length );
+
+        string temp = pettingZoo[ r ];
+        pettingZoo[ r ] = pettingZoo[ i ];
+        pettingZoo[ i ] = temp;
+    }
+}
 
 string[,] AssignGroup( int groups = 6 )
 {
-    string[,] result = new string[groups, pettingZoo.Length / groups];
+    string[,] result = new string[ groups, pettingZoo.Length / groups ];
     int start = 0;
 
     for ( int i = 0; i < groups; i++ )
@@ -51,5 +68,4 @@ string[,] AssignGroup( int groups = 6 )
     }
 
     return result;
-
 }
